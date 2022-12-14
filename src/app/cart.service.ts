908047459fa316c18ec793c6e04415079674e0fb
nandimarqz <102648657+nandimarqz@users.noreply.github.com>
2022-12-14 13:30:58 +0100

    Gestion de datos(Carrito y gastos de envio)

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,16 +1,21 @@
 import { Product } from './products';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   items: Product[] = [];
-  constructor() {}
+  constructor(private http: HttpClient) {}
   addToCart(product: Product) {
     this.items.push(product);
   }
-
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
   getItems() {
     return this.items;
   }
